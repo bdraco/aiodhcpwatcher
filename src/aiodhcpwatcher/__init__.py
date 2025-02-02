6648a5926169c7f11d189cac95ff294e7f290897
J. Nick Koston <nick@koston.org>
2025-02-02 10:52:08 -0600

    chore: cover

--- src/aiodhcpwatcher/__init__.py
+++ src/aiodhcpwatcher/__init__.py
@@ -95,10 +95,9 @@ class AIODHCPWatcher:
 
     def restart_soon(self) -> None:
         """Restart the watcher soon."""
-        if self._restart_timer:
-            return
-        _LOGGER.debug("Restarting watcher in 30 seconds")
-        self._restart_timer = self._loop.call_later(30, self._execute_restart)
+        if not self._restart_timer:
+            _LOGGER.debug("Restarting watcher in 30 seconds")
+            self._restart_timer = self._loop.call_later(30, self._execute_restart)
 
     def _execute_restart(self) -> None:
         """Execute the restart."""
